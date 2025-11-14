2a5805d38da42428e3447c56536770aedf39e73c
Rios Razafinandriana <riosrazafinandriana@gmail.com>
2025-11-14 15:19:27 +0300

    add : CompareTheTriplets.java

---
+++ java-algorithms/CompareTheTriplets.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'compareTriplets' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER_ARRAY b
+     */
+
+    public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        // Code completed
+        List<Integer> result = new ArrayList<>();
+        Integer aPoints = 0;
+        Integer bPoints = 0;
+        
+        for(int i = 0; i < a.size(); i++){
+            if(a.get(i) != b.get(i)){
+                if(a.get(i) > b.get(i)){
+                    aPoints = aPoints + 1;
+                }else{
+                    bPoints = bPoints + 1;
+                }
+            }
+        }
+        
+        result.add(aPoints);
+        result.add(bPoints);
+        
+        return result;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] aTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> a = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            int aItem = Integer.parseInt(aTemp[i]);
+            a.add(aItem);
+        }
+
+        String[] bTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> b = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            int bItem = Integer.parseInt(bTemp[i]);
+            b.add(bItem);
+        }
+
+        List<Integer> result = Result.compareTriplets(a, b);
+
+        for (int i = 0; i < result.size(); i++) {
+            bufferedWriter.write(String.valueOf(result.get(i)));
+
+            if (i != result.size() - 1) {
+                bufferedWriter.write(" "); // we stop the blank space when we reach the last element
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
